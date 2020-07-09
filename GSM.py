d41f414fc007c5116a70613779cccaed82170318
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2020-07-09 18:50:47 +0200

    Update GSM.py

--- GSM.py
+++ GSM.py
@@ -1,6 +1,19 @@
 # Itti .al 1998 Saliency map implementation in Python3
 # Author : Munch Quentin
 
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
